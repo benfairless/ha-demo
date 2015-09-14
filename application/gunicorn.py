eaf81b64e2bab46a5fdc1bdfe266155623e430b8
Ben Fairless <ben.fairless@landregistry.gsi.gov.uk>
2015-09-14 13:47:17 +0100

    Enabled VIP and started HA Demo as service

--- application/gunicorn.py
+++ application/gunicorn.py
@@ -6,10 +6,12 @@ workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count() * 2
 
 chdir = os.path.dirname(os.path.realpath(__file__))
-pidfile = '/tmp/gunicorn.pid'
+pidfile = '/tmp/ha-demo.pid'
 
 accesslog = '/vagrant/tmp/access.log'
 access_log_format = '%(h)s %(l)s %(s)s %(l)s %(r)s'
 
 loglevel = 'info'
-errorlog = '/vagrant/tmp/error.log'
+#errorlog = '/vagrant/tmp/error.log'
+
+raw_env = [ 'BACKEND_HOST=backend-00', 'BACKEND_PORT=5000' ]
